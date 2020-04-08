64252e5a08e294055ee4aea0fa1c010a323b836b
Diana Cordero <dianaclxcoe@gmail.com>
2020-04-08 22:14:36 +0200

    .

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 function playsound(e) {
   const audio = this.document.querySelector(`audio[data-key='${e.keyCode}']`);
-  const key = this.document.querySelector(`.key[data-key='${e.keyCode}']`);
+  const key = this.document.querySelector(`div[data-key='${e.keyCode}']`);
   if (!audio) return;
+
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
@@ -15,6 +16,6 @@ function removeTransition(e) {
 }
 
 const keys = Array.from(document.querySelectorAll(".key"));
-keys.forEach((key) => key.addEventListener("transitioned", removeTransition));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
 window.addEventListener("keydown", playsound);
